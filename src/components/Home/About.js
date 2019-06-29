29da4b67e240240a2fa8d4c9b708bc07f178faac
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-06-30 00:00:49 +0530

    Linked the homepage button to about section

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -3,6 +3,7 @@ import Title from '../StyledTitle'
 import Styles from '../../css/about.module.css'
 import{useStaticQuery,graphql} from 'gatsby'
 import Img from 'gatsby-image'
+import {Link} from 'gatsby'
 
 const getAbout = graphql `
 query aboutImage{
@@ -31,7 +32,7 @@ const About = () => {
      <h5>A Not For Profit Organization</h5>
      <p>Unnati: The Knowledge Hub For Civil Aspirants is a not for profit organisation registered under the government of India. Started by Mr. Lokesh Chugh under the able guidance of Mr. Oscar Fernandez (MP, Rajya Sabha and Chief Patron, Unnati), Unnati is working for the upliftment of youth in different sectors from 18th of December, 2016. Unnati aims at providing free guidance to those UPSC aspirants who have knowledge and talent but lack adequate resources. </p>
      <p>We host various events and also provide free UPSC guidance to UPSC aspirants.</p>
-     <button type="button" className="btn-primary"> Read More</button>
+     <Link to="/about" className="btn-primary"> Read More</Link>
      </article>
      </div>
      </section>
