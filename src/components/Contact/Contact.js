6bedaf44c2200fc230cffb7d593250a67b1c89fd
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-06-30 00:17:01 +0530

    Fixed contact form and adjusted article card width

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,15 +10,15 @@ const Contact = () => {
     <form action="https://formspree.io/unnati.knowledge@gmail.com" method="POST" className={Styles.form}>
     <div>
     <label htmlFor="name">Name</label>
-    <input type="text" name="name" id="name" className={Styles.formControl} placeholder="Your name" />
+    <input type="text" name="name" id="name" className={Styles.formControl} placeholder="Your name" required />
     </div>
     <div>
     <label htmlFor="email">Email</label>
-    <input type="email" name="email" id="email" className={Styles.formControl} placeholder="email@example.com" />
+    <input type="email" name="email" id="email" className={Styles.formControl} placeholder="email@example.com" required />
     </div>
     <div>
     <label htmlFor="message">Message</label>
-    <textarea name="message" id="message" className={Styles.formControl} placeholder="Your Message" />
+    <textarea name="message" id="message" className={Styles.formControl} placeholder="Your Message" required/>
     </div>
     <div>
     <input type="submit" value="submit" className={Styles.submit} />
