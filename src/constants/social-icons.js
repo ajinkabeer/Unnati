44bc15b3264cb81ffdf8c8b5b649c4da057a3190
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-06-29 18:43:29 +0530

    Added Social Media Links

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -3,14 +3,14 @@ import { FaFacebook, FaTwitterSquare, FaInstagram } from "react-icons/fa"
 export default [
   {
     icon: <FaFacebook />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://www.facebook.com/unnaticell/",
   },
   {
     icon: <FaTwitterSquare />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://twitter.com/unnaticell",
   },
   {
     icon: <FaInstagram />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://www.instagram.com/unnati_cell/",
   },
 ]
