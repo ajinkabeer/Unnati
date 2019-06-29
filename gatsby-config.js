7d2a0b471b08a13b71d9d15dfc676899375b62d4
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-06-29 19:37:35 +0530

    Added Contentful CMS and set up the environment variables in netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
    siteMetadata:{
@@ -10,6 +13,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId:process.env.CONTENTFUL_SPACE_ID,
+        accessToken:process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`, `gatsby-plugin-sharp`,
