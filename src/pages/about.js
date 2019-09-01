26dd2b4f1eda790d0ee2cb171635efd60e76bc70
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-09-01 21:04:52 +0530

    Update about content

--- src/pages/about.js
+++ src/pages/about.js
@@ -36,18 +36,18 @@ const about = ({ data }) => {
           <div class="col-sm">
             <div style={{ width: 18 + "rem", textAlign: "center" }}>
               <div class="card-body">
-                <img src={Ritik} style={{ borderRadius: 30 }}></img>
-                <h5 class="card-title" style={{ marginTop: 20 }}>Mr.Ritik Shah</h5>
-                <p class="card-text" style={{ textAlign: "center" }}>Content Head</p>
+                <img src={Jyoti} style={{ borderRadius: 30 }}></img>
+                <h5 class="card-title" style={{ marginTop: 20 }}>Ms.Jyoti Gupta</h5>
+                <p class="card-text" style={{ textAlign: "center" }}>President</p>
               </div>
             </div>
           </div>
           <div class="col-sm">
             <div style={{ width: 18 + "rem", textAlign: "center" }}>
               <div class="card-body">
-                <img src={Jyoti} style={{ borderRadius: 30 }}></img>
-                <h5 class="card-title" style={{ marginTop: 20 }}>Ms.Jyoti Gupta</h5>
-                <p class="card-text" style={{ textAlign: "center" }}>President</p>
+                <img src={Ritik} style={{ borderRadius: 30 }}></img>
+                <h5 class="card-title" style={{ marginTop: 20 }}>Mr.Ritik Shah</h5>
+                <p class="card-text" style={{ textAlign: "center" }}>Content Head</p>
               </div>
             </div>
           </div>
