8f0ed08b6c413a20d82a16ddcfb14736edf536c9
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-11-14 17:01:56 +0530

    Test new gallery

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,57 +1,44 @@
 import React from "react"
 import styles from "../../css/single-blog.module.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import GooglePhoto from "react-google-photo"
+import ImageGallery from "react-image-gallery"
 
 const parliamentaryDebate = [
   {
-    src:
-      "../../images/parliamentary debate/0f54a95d-955b-4fa6-966a-5d267a8a25a7.jpg",
-    width: 749, // width of your image
-    height: 1000, // height of your image
+    original:
+      "https://lh3.googleusercontent.com/9uT1bOo-ybgXSF2SmwxhtfYg4tt6HT1TSuAdDuocWe4ij2zHUcikENoQIdk6ZLwma8enOAGzdxShuzaoLbhcnTPln4wzOGH7TpZb_oyZ_RYJ64uz3ledGW8ztOWAitFr2C7NKECMpHPDcZcZNsAor3aMSkdBYm_qlHAKcDx41EKrGSV8290BuX7TLweh2uZTaU3Wksegan2x57l7Q_cXY1-it-hrMV9KJ1Qiu5RRa2HOiCrOH0cMOJl7wzb9WwAkfdER0mBCJVgk7-A1vzNKXPSEOb4FCEsuLByUGdXTcDuz1xB2MorBbmdRFR0kNs5o5rrBoBVyqg1lI-HNCxxBqPY6aV3pSVcxZzW7HyTECrJVDj3ow8kNUzgfMgtUyIul47LkVdbp_uu1ANx1jDBYrfOFjPGSD9bB2DScwrduU0GlifPUy3_LkExx1P7EqD9rCcxWyLa3e9pcDjiqfnVKRKnRCFjTqdY0ZBIg-WXuKRwndiGCatNxTBFn5jAZECOIsLmrdnNuszwMoZCpIjg3F0p-0x82EAYtzf2vRkkmIZ1eM8oQghl7HF9iwcUqMBixkyeApsLVkZxVsPnK-D3Y7k7KzmbRfYxzfBr4St5NbAwPi-YClhyELkr8xu13pxsgkoPDpKdkzkHqGvIIaaBkaWtapsKhrURFVhvXbTjxZCu2ekEqI-a5pYoU61jrMGADNafoaYbxI5msfv7B1L5UwGiHJ89ChWBhVCvqjrHvqY8Ccg=w348-h260-no",
+      sizes : {
+        width:'250',
+        height:'250'
+      }
   },
   {
-    src:
-      "../../images/parliamentary debate/8484e1f4-aa53-4188-9433-99d68be24ee1.jpg",
-    width: 749, // width of your image
-    height: 1000, // height of your image
+    original:
+      "https://lh3.googleusercontent.com/OXoFtnKMQHRJIBHBfXqVxR-X-E4rOKnxrrxZfR-H-9C1qKisgzaXpFI-yrE-vDcZuAS8HPYFwo21xL2Z__kmiK5_HgF_PmD8yh6Br0UeWe67eO6gC94pbY-odnVkYAZyfm1MpcntNEPegU0RkCjNQYBzx3yIu_rYA3egXqfruGVb_BFH9m9hu8mEB1nrAEzMl_kbU-n7tB_cx946tiiZ6ynhEP5LZZZNmQ63ebZhoDYkmjYdVIT13dTfPKg2DaaE5mfnXHJUfjNbi50t2EX5Ev6HDf0ujaZuj9_ZSo3ViDlHiLl8icUZ1gXMobZg9nrC3yRw9QKGXqHpt4pGuqxdLXTW9OXM11ekdkxFzYr1WLZ5VOnuOdbikTfm8AsIemc6xZZz9tjVn7dx1yCMIDL-zEO1HyAtyq_xM2uQks1owe3Zz6tRn6652knZtArAOv1ZKm5LuLaIlWckl7hHp1JknuBOlxwQ2TIR18ih80_r6UnXsONWwMhN74CRmXUYwSVooU0IOJoWPMITMWMASQhkkNO71Opxh7LZIZzg-Cz9mYCcapI2usnrI14DNcA3VPRsJNmyyaSeSGoOy2c2UYv2a_2rOF-Qp9chcNzHexPctGixnLm4P5qAGpkh1wX-jEvYkFpzmZrJqchGNZCIon8s3zeBde9TWGzoPFgPfx13r8YuzQUJpE9xcE4_4qRrS3huYk3eKrpVFgm1xjsVTrQ753jkTfMDhpjk_mZbYBmAArp7gg=w348-h260-no",
+      sizes : {
+        width:'250',
+        height:'250'
+      }
+    },
+  {
+    original:
+      "https://lh3.googleusercontent.com/vESkfl9mMxwzvhX8iVwEKEwrc_L3QUyEfRxji7P_FPBw6vk0ZZ2kZ2NY5H7UU4uZTQePH4WO3GrRJp8bpqDOqM6TDs06NuMBTZDFz2FnpA4S12H00qirEFidadJ91SqMOhCrORmqZcaKIZX07ACUJtDbcUARVNrS_vkfexrpBFhfhfcpXD1bXUx4k6dmFNtDMgD0HsAl5Vb21AJCLG9f3SiRFVqAaw_sDWfsjMu-IPRaQWFNhM-kuzl5D3AwcG-g-BCh2sqK9TVxwCyn4KhyICIfHe_p7yQulV_7SiJGLAsvqONFPkTt29uSTBfSVs0lqMZ_Z2zE_YZ8qAQCuA4GZYhe2uq1Z-aACgPCiyaMn3LuM5GouHZjuErvTWqM4vAt0MnIVNkwak34TaBZ62bCFde63qaK0RCdzJYZKzAlVU4GHOZKMFY12XpZtyAzWJ1cIWRIdwuEIdweH0QoWxSxCgw_uVn6Z06IL7s_5_jlNNxp3ipcqgE05DBnszX7GdVB32-b_hbv4PAdvnnRlcfxkIUhDg-XbwZTuTiaTnqtkWuFbQjDj7h9YMuwH60kKHMMLeqqo9Ttt0gQas2CFpm1WqL0Acnks_HAo6v_v4bBatRpVZhQkiZgfI2WO5oo-Aj24vjpS-EBlAvVX-F7fMflbjErznNUkp5j6oxujNCMgeXp2awg5KLso-K8LAyP1covYeWaUyNnacQzCLH7lhp_faWieX5v_FwR-Sqo6YhpK3ljpg=w348-h260-no",
+      sizes : {
+        width:'250',
+        height:'250'
+      }
   },
   {
-    src:
-      "../../images/parliamentary debate/97a87c20-1134-4e0c-8fd6-6cfc6f17728b.jpg",
-    width: 749, // width of your image
-    height: 1000, // height of your image
-  },
-  {
-    src:
-      "../../images/parliamentary debate/cc8ad971-5b26-40a7-b358-787a4a8b1a55.jpg",
-    width: 749, // width of your image
-    height: 1000, // height of your image
+    original:
+      "https://lh3.googleusercontent.com/JzZiLDF-2UK42WYAmtIn9RKFT8A1JvxpclkXGKq7w9-DygG8LdgxWim2KCEVcjE17mTjY2wuXZKdk16cxG6gC3YZTIXvkwvWgx2cXptA-3szgHsJ5sylihzBSRzO0N28GY3Q9FcXi6okPTxvCYpJ3NoUGtxpt_9QExpT37Rv0Jo7Od2ORltBkVD0kVcU29oKgAmQrg4YFRW320vwfCQCJMU9tJ0q-JUdHsOsrueuhejQyrDw1B__td5DLeNn29yI55hGZm0aa3SHrnrEtQ4OcWwaCXTZK2yg7kUG3p9cOxOBjOe2w9ioI17Aw25p-7X7ZkiL_NCURoNIhGcDukhmQlKlUyxZzwLteamInEE377RlrUsGVC90zzmR0bpPP_QVC4P_Ynjd9LMj3Rxj7A9GU8eGnV01sgUnsZ5BDbbJ5OuA7Pd3qbXugCZVYEyUwiQK7qPhaUoP2FryTaLtWEFih8ZrJIAtTOYGzfNGngKFIVEeMIytzgEPl_xtl-p6ov9AaTd1OMC_tcamOyUmNe8mFpbatE5hpHKfoQk4-v7rKDWbmI-yHbjHQcOwGq9oxkoDNA8I-35VpDmhmtXsp-X44MXpjOatbxaSFwuMO6SbUgZmW5Rl-IBV4bFkZLLk6OZzHLIsD__P-tZq8_w_v4CL5cHv50Ayx0ks6D3lDLAHO66Uex0-8lyXuwfXTg6VyQ7BFUF_GC2o9y46Qcli1dtHCMQ4Xtc2n-9vtxZmmzfkq8Q9hA=w348-h260-no",
+      sizes : {
+        width:'250',
+        height:'250'
+      }
   },
 ]
 
 class events extends React.Component {
-  state = {
-    index: 0,
-    open: false,
-  }
-
-  handleClickPrev = () => {
-    this.setState({ index: this.state.index - 1 })
-  }
-
-  handleClickNext = () => {
-    this.setState({ index: this.state.index + 1 })
-  }
-
-  handleOpen = () => {
-    this.setState({ open: true })
-  }
-
-  handleClose = () => {
-    this.setState({ open: false })
-  }
-
   render() {
     return (
       <div>
@@ -73,15 +60,7 @@ class events extends React.Component {
                   than 1300 students.
                 </p>
               </article>
-              <button onClick={this.handleOpen}>Open</button>
-              <GooglePhoto
-                open={this.state.open}
-                src={parliamentaryDebate}
-                srcIndex={this.state.index}
-                onClickPrev={this.handleClickPrev}
-                onClickNext={this.handleClickNext}
-                onClose={this.handleClose}
-              />
+              <ImageGallery items={parliamentaryDebate}  showPlayButton={false} style={{width:'50%'}} showFullscreenButton={false}/>
             </div>
           </div>
         </div>
