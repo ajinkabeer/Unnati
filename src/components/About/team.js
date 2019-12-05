15601ba2be3347739abb0a9e6d35fb2bf727e4e6
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-12-05 16:06:26 +0530

    Test team component spacing

--- src/components/About/team.js
+++ src/components/About/team.js
@@ -1,47 +1,57 @@
 import React from "react"
-import './team.css'
+import "./team.css"
 import Lokesh from "../../images/lokesh.jpg"
 import Jyoti from "../../images/jyoti.jpg"
-import oscar from '../../images/oscar.jpg'
-import punia from '../../images/punia.jpg'
+import oscar from "../../images/oscar.jpg"
+import punia from "../../images/punia.jpg"
 
 const team = () => {
   return (
     <div class="row">
       <div class="column">
         <div class="card">
-          <img src={oscar} alt="oscar fernandes" style={{width:"100%"}} />
+          <img src={oscar} alt="oscar fernandes" style={{ width: "100%" }} />
           <div class="container">
-            <h2 style={{fontSize:17,marginTop:5}}>Mr. Oscar Fernandes</h2>
-            <p class="title">Chief Patron of Unnati (Member of Parliament, Rajya Sabha)</p>
+            <h2 style={{ fontSize: 17, marginTop: 5 }}>Mr. Oscar Fernandes</h2>
+            <p class="title">
+              Chief Patron of Unnati
+            </p>
+            <p>(Member of Parliament, Rajya Sabha)</p>
           </div>
         </div>
       </div>
 
       <div class="column">
         <div class="card">
-          <img src={punia} alt="Mr. P. L. Punia" style={{width:"100%"}} />
+          <img src={punia} alt="Mr. P. L. Punia" style={{ width: "100%" }} />
           <div class="container">
-            <h2 style={{fontSize:17,marginTop:5}}>Mr. P. L. Punia</h2>
-            <p class="title">Chief Patron of Unnati (Member of Parliament, Rajya Sabha)</p>
+            <h2 style={{ fontSize: 17, marginTop: 5 }}>Mr. P. L. Punia</h2>
+            <p class="title">
+              Chief Patron of Unnati 
+            </p>
+            <p>(Member of Parliament, Rajya Sabha)</p>
           </div>
         </div>
       </div>
       <div class="column">
         <div class="card">
-          <img src={Lokesh} alt="Lokesh" style={{width:"100%"}} />
+          <img src={Lokesh} alt="Lokesh" style={{ width: "100%" }} />
           <div class="container">
-            <h2 style={{fontSize:17,marginTop:5}}>Mr.Lokesh Chugh</h2>
-            <p class="title">Founder and Chairman</p>
+            <h2 style={{ fontSize: 17, marginTop: 5 }}>Mr.Lokesh Chugh</h2>
+            <p style={{ paddingBottom: 20 }} class="title">
+              Founder and Chairman
+            </p>
           </div>
         </div>
       </div>
       <div class="column">
         <div class="card">
-          <img src={Jyoti} alt="jyoti" style={{width:"100%"}} />
+          <img src={Jyoti} alt="jyoti" style={{ width: "100%" }} />
           <div class="container">
-            <h2 style={{fontSize:20,marginTop:5}}>Ms. Jyoti Gupta</h2>
-            <p class="title">President</p>
+            <h2 style={{ fontSize: 17, marginTop: 5 }}>Ms. Jyoti Gupta</h2>
+            <p style={{ paddingBottom: 20 }} class="title">
+              President
+            </p>
           </div>
         </div>
       </div>
