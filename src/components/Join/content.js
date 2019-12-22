ef9c588b36df67d9f0262514e20498ee1cf50e65
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-12-22 09:58:56 +0530

    Fix join component error

--- src/components/Join/content.js
+++ src/components/Join/content.js
@@ -1,23 +1,41 @@
-import React, { Component } from "react"
-import Layout from "../components/Layout"
-import { graphql } from "gatsby"
-import Title from "../components/StyledTitle"
-import Styles from "../css/about.module.css"
-import Abouts from "../components/About/abouts"
-import Homeabout from "../components/Home/About"
-import Styles2 from "../css/single-blog.module.css"
+import React from "react"
+ import { graphql } from "gatsby"
+import Styles from "../../css/about.module.css"
+import Styles2 from "../../css/single-blog.module.css"
 import ImageGallery from "react-image-gallery"
 import { campus, poster } from "./links"
 
 const content = () => {
   return (
-    <Layout>
-      <section className={Styles.about}>
+    <>
+    <div className={Styles2.center}>
+          <article className={Styles2.article}>
+            <p>
+              "The only person you are destined to become is the person you
+              decide to be.” – Ralph Waldo Emerson. To stand out of crowd you
+              need to put your best foot forward. Internship gives you an
+              opportunity to learn things practically while you are still in
+              college. Team Unnati participated in internship fair of different
+              colleges to provide them an opportunity to learn the work culture
+              while they are still in college and sharpen their skills. Unnati:
+              The Knowledge Hub For Civil Aspirants is a not for profit
+              organisation registered under the government of India. Started by
+              Mr. Lokesh Chugh under the able guidance of Mr. Oscar Fernandez
+              (MP, Rajya Sabha and Chief Patron, Unnati), Unnati is working for
+              the upliftment of youth in different sectors from 18th of
+              December, 2016. Unnati aims at providing free guidance to those
+              UPSC aspirants who have knowledge and talent but lack adequate
+              resources. We host various events and also provide free UPSC
+              guidance to UPSC aspirants.
+            </p>
+          </article>
+        </div>
+       <section className={Styles.about}>
         <div className={Styles.aboutCenter}>
           <article className={Styles.aboutImg}>
             <div className={Styles.imgContainer}>
               <ImageGallery
-                items={poster}
+                items={campus}
                 showPlayButton={false}
                 showFullscreenButton={false}
                 showIndex={false}
@@ -26,13 +44,16 @@ const content = () => {
             </div>
           </article>
           <article className={Styles.aboutInfo}>
-            <h5>Internships</h5>
-            <a
-              className="btn-primary"
-              href="https://docs.google.com/forms/d/e/1FAIpQLSfPJRwHPGrQVe9j6b0W7DWd6b-m3mabeTu73BFP9z5GtUm97A/viewform"
-            >
-              Click here to proceed
-            </a>
+          <h5>Unnati on Campus</h5>
+            <p>
+              "The only person you are destined to become is the person you
+              decide to be.” – Ralph Waldo Emerson. To stand out of crowd you
+              need to put your best foot forward. Internship gives you an
+              opportunity to learn things practically while you are still in
+              college. Team Unnati participated in internship fair of different
+              colleges to provide them an opportunity to learn the work culture
+              while they are still in college and sharpen their skills.
+            </p>
           </article>
         </div>
       </section>
@@ -60,7 +81,7 @@ const content = () => {
           </article>
         </div>
       </section>
-    </Layout>
+    </>
   )
 }
 
