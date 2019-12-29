2c23b01978a360924aa5a62087b20cc31d7e181c
Ajin Kabeer <ajiinkabeer@gmail.com>
2019-12-29 17:45:21 +0530

    Refactor article.js

--- src/pages/article.js
+++ src/pages/article.js
@@ -3,24 +3,12 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import BlogList from "../components/Blog/BlogList"
 
-const blog = ({ data }) => {
+const blog = () => {
   return (
     <Layout>
       <BlogList />
     </Layout>
   )
 }
-
-export const query = graphql`
-  query {
-    defaultBcg: file(relativePath: { eq: "artic.jpeg" }) {
-      childImageSharp {
-        fluid(quality: 90, maxWidth: 4160) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-  }
-`
 
 export default blog
