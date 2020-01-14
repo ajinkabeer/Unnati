9e0762196cd017991ac723812871845eb902d3a8
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-01-14 16:51:51 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: "process.env.CONTENTFUL_SPACE_ID",
-        accessToken: "",
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`,
