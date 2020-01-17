d965e3b17d4c810a44438d6bf43429c8da4e1b7c
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-01-17 07:05:42 +0530

    Update journey.js

--- src/components/About/journey.js
+++ src/components/About/journey.js
@@ -10,32 +10,6 @@ const journeySoFar = () => {
         <article className={Styles.aboutImg}>
           <div>
             <ImageGallery
-              items={journey}
-              showPlayButton={false}
-              showFullscreenButton={false}
-              showIndex={false}
-              infinite={false}
-            />
-          </div>
-        </article>
-        <article className={Styles.aboutInfo}>
-          <h5>Journey so far</h5>
-          <p>
-            {" "}
-            It seems like just now, only a few days ago UNNATI was formed but it
-            has been a whole year. We cannot believe how far we have come in
-            just 365 days, all credits to the love and support you guys have
-            shown. We look back to the past 2 years with an awe. How amazing it
-            has been! How much we have grown up in just 2 years! We have you
-            guys to thank for it. Let's promise to continue the growth and reach
-            impeccable heights
-          </p>
-        </article>
-      </div>
-      <div className={Styles.aboutCenter} style={{ marginTop: 100 }}>
-        <article className={Styles.aboutImg}>
-          <div>
-            <ImageGallery
               items={journey2}
               showPlayButton={false}
               showFullscreenButton={false}
@@ -45,7 +19,7 @@ const journeySoFar = () => {
           </div>
         </article>
         <article className={Styles.aboutInfo}>
-          <h5>Third Birthday!</h5>
+          <h5>Journey so far</h5>
           <p>
             {" "}
             Alone we can do so little, together we can do so much. Unnati is not
